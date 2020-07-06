eb134561d7d287de26ef5ebe546b9b6f62075a95
Abel Shields <abeshi@intabeshi.home>
2020-07-06 15:00:24 +0100

    Tests all completed up to 17

--- app/src/test/java/com/example/fizzbuzz/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fizzbuzz/ExampleUnitTest.kt
@@ -15,4 +15,43 @@ class FizzBuzzTest {
         // Just to test that the function is producing some output.
         assertEquals("1", fizzbuzz(1))
     }
+
+    @Test
+    fun fizzbuzz_basic_mul35() {
+        // Default fizzbuzz behaviour.
+        assertEquals("fizz", fizzbuzz(3))
+        assertEquals("buzz", fizzbuzz(5))
+        assertEquals("fizzbuzz", fizzbuzz(3*5))
+    }
+
+    @Test
+    fun fizzbuzz_extended_mul7() {
+        // Each multiple of 7 should append "bang"
+        assertEquals("fizzbang", fizzbuzz(3 * 7))
+        assertEquals("buzzbang", fizzbuzz(5 * 7))
+        assertEquals("fizzbuzzbang", fizzbuzz(3 * 5 * 7))
+    }
+
+    @Test
+    fun fizzbuzz_extended_mul11() {
+        // Each multiple of 11 should return just "bong"
+        assertEquals("bong", fizzbuzz(11))
+        assertEquals("bong", fizzbuzz(5 * 11))
+    }
+
+    @Test
+    fun fizzbuzz_extended_mul13() {
+        // Each multiple of 13, put Fezz before last "b-word" -- even if 11 is present.
+        assertEquals("fezz", fizzbuzz(13))
+        assertEquals("fezzbuzz", fizzbuzz(65))
+        assertEquals("fizzfezzbuzz", fizzbuzz(3 * 5 * 13))
+        assertEquals("fezzbong", fizzbuzz(11 * 13))
+    }
+
+    @Test
+    fun fizzbuzz_extended_mul17() {
+        // Reverse the order of the output
+        assertEquals("buzzfizz", fizzbuzz(3 * 5 * 17))
+        assertEquals("buzzfezzfizz", fizzbuzz(3 * 5 * 13 * 17))
+    }
 }
