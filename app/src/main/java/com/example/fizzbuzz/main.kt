80b2d95c05deee5eb61e6fda775b8fe81cad0831
Abel Shields <abeshi@intabeshi.home>
2020-07-06 14:38:46 +0100

    First working fizzbuzz

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,23 @@
 package com.example.fizzbuzz
 
+import kotlin.text.StringBuilder
+
 fun main() {
-    print("Hello world!")
+    println("Hello world!")
+
+    val maxVal = 100
+    for (index in 1..maxVal) {
+        val builder = StringBuilder()
+        if (index % 3 == 0)
+            builder.append("fizz")
+        if (index % 5 == 0)
+            builder.append("buzz")
+        // Can be extended...
+
+        val fbString = builder.toString()
+        if (fbString.length != 0)
+            println(fbString)
+        else
+            println(index)
+    }
 }
